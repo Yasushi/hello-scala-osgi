027a51bd55b3234c054f85d40803743db7c7a262
Yasushi <yasushi.abe@gmail.com>
2009-05-14 13:09:13 +0900

    Register service by scalamodules.

--- src/main/scala/com/domain/osgi/scala/internal/HelloScalaActivator.scala
+++ src/main/scala/com/domain/osgi/scala/internal/HelloScalaActivator.scala
@@ -1,17 +1,16 @@
 package com.domain.osgi.scala.internal
 
 import org.osgi.framework._
-import org.osgi.service.http._
 import com.domain.osgi.scala.HelloScalaService
+
+import org.scalamodules.core.RichBundleContext.toRichBundleContext
 
 class HelloScalaActivator extends BundleActivator {
   var serviceRegistration:ServiceRegistration = _
-  var httpService:HttpService = _
 
   def start(context: BundleContext) {
     Console.println("STARTING com.domain.osgi.scala")
-    serviceRegistration = context.registerService("com.domain.osgi.scala.HelloScalaService",
-    		new HelloScalaServiceImpl(), null)
+    serviceRegistration = context registerAs classOf[HelloScalaService] theService new HelloScalaServiceImpl()
     Console.println("REGISTERED com.domain.osgi.scala.internal.HelloScalaService")
   }
 
