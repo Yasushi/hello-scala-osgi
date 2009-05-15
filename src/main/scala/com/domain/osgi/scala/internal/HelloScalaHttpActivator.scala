568025831fedad401343e94c5bba28f0736a14bd
Yasushi <yasushi.abe@gmail.com>
2009-05-15 13:34:28 +0900

    fix using HttpService.
    getOne is ensure ungetService. HttpService will remove all servlets
    and resources from itself when ungetService.

--- src/main/scala/com/domain/osgi/scala/internal/HelloScalaHttpActivator.scala
+++ src/main/scala/com/domain/osgi/scala/internal/HelloScalaHttpActivator.scala
@@ -9,27 +9,37 @@ import java.util.Hashtable
 
 class HelloScalaHttpActivator extends BundleActivator {
 
+	var httpServiceRef:ServiceReference = _
   
     def start(context: BundleContext) {
-    	context getOne classOf[HttpService] andApply { httpService =>
-    	val httpContext = httpService.createDefaultHttpContext()
-    	val initParams = new Hashtable[String, String]()
-    	initParams.put("from", "HttpService")
-    	httpService.registerServlet("/helloworld/hs",
-    			new HelloWorldServlet("/helloworld/hs"),
-    			initParams,
-    			httpContext)
-    	httpService.registerServlet("/",
-    			new HelloWorldServlet("/"),
-    			initParams,
-    			httpContext)
-    	httpService.registerResources("/images","/images",httpContext)
-    	Console.println("STARTING")
-    	}
+      	httpServiceRef = context.getServiceReference(classOf[HttpService].getName)
+      	httpServiceRef match {
+      	  case null => Console.println("HttpService reference not found.")
+      	  case ref =>
+      	    httpServiceRef = ref
+      	    context.getService(httpServiceRef) match {
+      	      case null => Console.println("HttpService not found.")
+      	      case httpService:HttpService =>
+      	      val httpContext = httpService.createDefaultHttpContext()
+      	      val initParams = new Hashtable[String, String]()
+      	      initParams.put("from", "HttpService")
+      	      httpService.registerServlet("/helloworld/hs",
+      	    		  new HelloWorldServlet("/helloworld/hs"),
+      	    		  initParams,
+      	    		  httpContext);
+      	      httpService.registerServlet("/",
+      	    		  new HelloWorldServlet("/"),
+      	    		  initParams,
+      	    		  httpContext);
+      	      httpService.registerResources("/images","/images",httpContext)
+      	    }
+      	}
     	Console.println("STARTED com.domain.osgi.scala")
     }
    
     def stop(context: BundleContext) {
+    	if (httpServiceRef != null)
+    		context.ungetService(httpServiceRef)
     	Console.println("STOPPED com.domain.osgi.scala")
     }
 }
